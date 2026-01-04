c7c9f649346b286aab97dcd9047d99acc0bd7864
ka1ne <emailkaine@gmail.com>
2026-01-04 12:43:22 +0000

    + config

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.opt.wrap = true -- Enable line wrapping
+vim.opt.linebreak = true -- Wrap lines at convenient points (words)
+vim.opt.breakindent = true -- Wrapped lines will maintain the same indent
